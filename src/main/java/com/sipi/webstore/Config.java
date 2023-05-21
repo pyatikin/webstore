a9bf0bde701d87ff8092e3e15d1f72fda51338ba
nikita <pyatkin.nikitka@mail.ru>
2023-05-21 23:57:20 +0300

    commandLiner improve

--- src/main/java/com/sipi/webstore/Config.java
+++ src/main/java/com/sipi/webstore/Config.java
@@ -19,23 +19,24 @@ import java.util.List;
 public class Config {
     @Bean
     public CommandLineRunner commandLineRunner(OrderRepository orderRepository, ProductRepository productRepository) {
+
         ClassLoader classLoader = getClass().getClassLoader();
         File file1 = new File(classLoader.getResource("food1.png").getFile());
         byte[] bFile1 = new byte[(int) file1.length()];
-        File file2 = new File(classLoader.getResource("food2.png").getFile());
-        byte[] bFile2 = new byte[(int) file2.length()];
-        try (FileInputStream fileInputStream1 = new FileInputStream(file1);
-             FileInputStream fileInputStream2 = new FileInputStream(file2)) {
+        /*File file2 = new File(classLoader.getResource("food2.png").getFile());
+        byte[] bFile2 = new byte[(int) file2.length()];*/
+        try (FileInputStream fileInputStream1 = new FileInputStream(file1)/*;
+             FileInputStream fileInputStream2 = new FileInputStream(file2)*/) {
             fileInputStream1.read(bFile1);
-            fileInputStream2.read(bFile2);
+            /*fileInputStream2.read(bFile2);*/
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
         return args -> {
             orderRepository.save(new Orders(1, 1, 1, 1, 1, 1, new Date(), "123"));
             productRepository.saveAll(List.of(
-                    new Product(1, Base64.getEncoder().encodeToString(bFile1), "Food1", "", 100),
-                    new Product(2, Base64.getEncoder().encodeToString(bFile2), "Food2", "", 200)
+                    new Product(1, Base64.getEncoder().encodeToString(bFile1), "Food1", "", 100)/*,
+                    new Product(2, Base64.getEncoder().encodeToString(bFile2), "Food2", "", 200)*/
             ));
         };
     }
